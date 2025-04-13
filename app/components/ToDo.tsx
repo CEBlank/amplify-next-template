1f5d503ec595f55060e681abb8f5c1544d7c346d
Chris <96022511+CEBlank@users.noreply.github.com>
2025-04-13 18:18:38 -0500

    Lab 08 Initial Push
    getting error " NoValidAuthTokens: No federated jwt " in local host.

---
+++ app/components/ToDo.tsx
@@ -0,0 +1,53 @@
+"use client"; 
+// import { useAuthenticator } from "@aws-amplify/ui-react";
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "../../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    
+  // const { signOut, user } = useAuthenticator();
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
+  }
+
+  return (
+    <>
+      <h1>Chris's todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
+      </ul>
+    </>
+  );
+}
