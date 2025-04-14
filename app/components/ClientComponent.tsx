cf5edf04815d3a07fb6a43ce91a236393cc03096
CEBlank <96022511+CEBlank@users.noreply.github.com>
2025-04-14 02:49:18 -0500

    Project 4
    Client side Weather Data

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -4,12 +4,20 @@ import { useEffect, useState } from "react";
 
 export default function LocationFinderClient() {
     const [locationInfo, setLocationInfo] = useState({City: "N/A"});
-
+    const [localWeather, setLocalWeather] = useState({});
+    
     const getLocationInfo = async () => {
       const response = await fetch("https://apip.cc/json");
       const locationData = await response.json();
       setLocationInfo(locationData);
       console.log(response);
+
+        const res = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0");
+        const weatherData = (await res.json());
+        setLocalWeather(weatherData);
+        console.log(weatherData);
+        console.log(localWeather);
+
 
     };
 
@@ -19,7 +27,9 @@ export default function LocationFinderClient() {
 
 return (
     <>
-        <h1>Client Component: Hello from {locationInfo.City}!</h1>
+        <h4>Client Component</h4>
+        <h2>Hello from {locationInfo.City}!</h2>
+       {/*  <p>The temperature in {locationInfo.City} is {localWeather}</p> */}
     </>
 );
 }
