da26624dfd2487513d5432d8b8799e2124a8c1a2
CEBlank <96022511+CEBlank@users.noreply.github.com>
2025-04-15 00:32:11 -0500

    Update ClientComponent.tsx

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -4,23 +4,36 @@ import { useEffect, useState } from "react";
 import Loading from "../loading";
 
 export default function LocationFinderClient() {
-    const [locationInfo, setLocationInfo] = useState({City: "N/A"});
+    const [locationInfo, setLocationInfo] = useState({"City" : "N/A"});
     const [localWeather, setLocalWeather] = useState(0);
     const [loadingGlimmer, setLoading] = useState(true);
-    const [localFahrenheit, setFahrenheit] = useState(0);
+    //const [localFahrenheit, setFahrenheit] = useState(0);
     
     const getLocationInfo = async () => {
       //await new Promise((resolve) => setTimeout(resolve, 2000));
       const response = await fetch("https://apip.cc/json");
+
       const locationData = await response.json();
+
       setLocationInfo(locationData);
-     // console.log(response);
 
-        const res = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0");
-        const weatherData = (await res.json()).dataseries;
-        setLocalWeather(weatherData[0].temp2m);
+      console.log(locationInfo);
+      console.log(locationData.City)
+    
 
-        setFahrenheit((localWeather * 9/5) + 32);
+        //weather based on location data API 
+        const url = "https://www.7timer.info/bin/astro.php?lon=" 
+                    + locationData?.Longitude
+                    + "$lat="
+                    + locationData?.Latitude
+                    + "&ac=0&unit=metric&output=json&tzshift=0";
+
+        const res = await fetch(url);
+        const weatherData = await res.json();
+
+        setLocalWeather(weatherData?.dataseries[0]?.temp2m);
+
+        //setFahrenheit((localWeather * 9/5) + 32);
 
         setLoading(false);
     };
@@ -37,7 +50,7 @@ return (
             <>
                 <h4>Client Component:</h4>
                 <h3>Hello from {locationInfo.City}!</h3>
-                <p>The temperature in {locationInfo.City} is {localWeather} degrees Celsius, {localFahrenheit} degrees Fahrenheit.</p> 
+                <p>The temperature in {locationInfo.City} is {localWeather} degrees Celsius. {/* {localFahrenheit} degrees Fahrenheit. */}</p> 
          </>
         )}
     </>
