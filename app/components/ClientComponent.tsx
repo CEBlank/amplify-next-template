c413593f0c6d0f32b1615d3a01c5ad6dc126222c
CEBlank <96022511+CEBlank@users.noreply.github.com>
2025-04-14 22:00:44 -0500

    Client Component temperature added

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
 export default function LocationFinderClient() {
     const [locationInfo, setLocationInfo] = useState({City: "N/A"});
-    const [localWeather, setLocalWeather] = useState({});
+    const [localWeather, setLocalWeather] = useState(null);
+    const [temperatureConvert, setConversion] = useState(0);
+    
     
     const getLocationInfo = async () => {
       const response = await fetch("https://apip.cc/json");
@@ -13,12 +15,10 @@ export default function LocationFinderClient() {
       console.log(response);
 
         const res = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0");
-        const weatherData = (await res.json());
-        setLocalWeather(weatherData);
-        console.log(weatherData);
-        console.log(localWeather);
-
-
+        const weatherData = (await res.json()).dataseries;
+    
+        setLocalWeather(weatherData[0].temp2m);
+        
     };
 
     useEffect(() => {
@@ -29,7 +29,7 @@ return (
     <>
         <h4>Client Component</h4>
         <h2>Hello from {locationInfo.City}!</h2>
-       {/*  <p>The temperature in {locationInfo.City} is {localWeather}</p> */}
+        <p>The temperature in {locationInfo.City} is {localWeather} degrees Celcius </p> 
     </>
 );
 }
