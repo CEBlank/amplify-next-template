ae9d7fdbceed33d6a5d8924d7fe75c178762ec6a
CEBlank <96022511+CEBlank@users.noreply.github.com>
2025-04-14 23:28:50 -0500

    Server Component Temp
    tweaks to Client component
     fahrenheit conversion
    Server component temp w/conversion added

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -5,22 +5,23 @@ import Loading from "../loading";
 
 export default function LocationFinderClient() {
     const [locationInfo, setLocationInfo] = useState({City: "N/A"});
-    const [localWeather, setLocalWeather] = useState(null);
+    const [localWeather, setLocalWeather] = useState(0);
     const [loadingGlimmer, setLoading] = useState(true);
-    
+    const [localFahrenheit, setFahrenheit] = useState(0);
     
     const getLocationInfo = async () => {
       await new Promise((resolve) => setTimeout(resolve, 2000));
       const response = await fetch("https://apip.cc/json");
       const locationData = await response.json();
       setLocationInfo(locationData);
-      console.log(response);
+     // console.log(response);
 
         const res = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0");
         const weatherData = (await res.json()).dataseries;
-    
         setLocalWeather(weatherData[0].temp2m);
-        
+
+        setFahrenheit((localWeather * 9/5) + 32);
+
         setLoading(false);
     };
 
@@ -35,8 +36,8 @@ return (
         ) : (
             <>
                 <h4>Client Component</h4>
-                <h2>Hello from {locationInfo.City}!</h2>
-                <p>The temperature in {locationInfo.City} is {localWeather} degrees Celsius </p> 
+                <h3>Hello from {locationInfo.City}!</h3>
+                <p>The temperature in {locationInfo.City} is {localWeather} degrees Celsius, {localFahrenheit} degrees Fahrenheit.</p> 
          </>
         )}
     </>
--- app/components/ServerComponent.tsx
+++ app/components/ServerComponent.tsx
@@ -4,9 +4,19 @@ export default async function LocationFinderServer() {
     const locationData = await response.json();
     const locationInfo = locationData;
 
+    const res = await fetch("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0");
+    const weatherData = (await res.json()).dataseries;
+    const localWeather = weatherData[0].temp2m;
+    const localFahrenheit = (localWeather * 9/5) + 32;
+
+
 return (
     <>
-        <h1>Server Component: Hello from {locationInfo?.City}!</h1>
+        <h4>Server Component:</h4>
+            <h3>Hello from {locationInfo?.City}!</h3>
+        <p>
+           The local temperature is {localWeather} degrees Celsius, {localFahrenheit} degrees Fahrenheit.
+        </p>
     </>
 );
 }
